b1d058d2c984ccc88f1d723e076b74ac1dcfccb7
Helga Gudmundsdottir <helgag@cs.washington.edu>
2017-10-16 20:10:01 -0700

    First pass

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # proxycache
+
+Cache HTTP(s) responses of a domain using a simple proxy.
+
+### Setup
+Uses Python 2.7 in a virtual environment
+
+```
+virtualenv .env
+source .env/bin/activate
+pip install -r requirements.txt
+```
+
+To run (in virtual env), change `BASE_DOMAIN` in proxy.py to desired domain.
+```
+python proxy.py
+```
---
+++ proxy.py
@@ -0,0 +1,98 @@
+import os
+import logging
+import re
+import BaseHTTPServer
+import hashlib
+import requests
+import werkzeug
+
+
+BASE_DOMAIN = 'https://thesite.com'
+PROXY_DOMAIN = 'http://localhost:8000'
+CACHE = True
+session = requests.Session()
+session.cookies = requests.cookies.cookielib.LWPCookieJar('cookies.txt')
+if not os.path.exists('cookies.txt'):
+    session.cookies.save()
+session.cookies.load()
+
+
+def transform_resp(data):
+    data = re.sub(r'(https?:\\/\\/[0-9a-z-.]+\.[a-z]{2,3})', r'http:\\/\\/localhost:8000/__dom/\1', data)
+    data = re.sub(r'(https?://[0-9a-z-.]+\.[a-z]{2,3})', r'http://localhost:8000/__dom/\1', data)
+    return data
+
+
+class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def _do_GET(self):
+        domain = re.findall(r'__dom/(https?://[0-9a-z-.]+\.[a-z]{2,3})', self.path)
+        if domain:
+            domain = domain[0]
+            path = self.path[7:]
+        else:
+            domain = BASE_DOMAIN
+            path = '{}{}'.format(BASE_DOMAIN, self.path)
+
+        hashfile = os.path.join('cache', hashlib.sha1(path).hexdigest() + '.cache')
+
+        if CACHE and os.path.exists(hashfile):
+            with open(hashfile) as f:
+                data = f.read()
+            self.send_response(200)
+        else:
+            r = session.get("{}".format(path))
+            data = transform_resp(r.content)
+            with open(hashfile, 'w') as f:
+                f.write(data)
+            self.send_response(r.status_code)
+
+        self.end_headers()
+        self.wfile.writelines(data)
+
+    def do_GET(self):
+        try:
+            self._do_GET()
+            session.cookies.save()
+        except Exception as e:
+            print("GET failed: %s\n%s" % (self.path, e))
+            logging.debug("GET failed: %s\n%s" % (self.path, e))
+
+    def _do_POST(self):
+        # TODO: cache this at all?
+        domain = re.findall(r'__dom/(https?://[0-9a-z-.]+\.[a-z]{2,3})', self.path)
+        if domain:
+            domain = domain[0]
+            path = self.path[7:]
+        else:
+            domain = BASE_DOMAIN
+            path = '{}{}'.format(BASE_DOMAIN, self.path)
+
+        length = int(self.headers.getheader('content-length'))
+        field_data = self.rfile.read(length)
+        fields = werkzeug.url_decode(field_data)
+
+        r = session.post("{}".format(path), data=dict(**fields))
+
+        data = transform_resp(r.content)
+
+        self.send_response(r.status_code)
+        self.end_headers()
+        self.wfile.writelines(data)
+
+    def do_POST(self):
+        try:
+            self._do_POST()
+            session.cookies.save()
+        except Exception as e:
+            print("POST failed: %s\n%s" % (self.path, e))
+            logging.debug("POST failed: %s\n%s" % (self.path, e))
+
+def run():
+    logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    server_address = ('', 8000)
+    httpd = BaseHTTPServer.HTTPServer(server_address, Handler)
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    run()
